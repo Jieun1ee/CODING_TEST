ce9ecd160e4ed5352181ed7319203ded1969424a
LEE JIEUN <112913242+Jieun1ee@users.noreply.github.com>
2024-01-10 23:08:53 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -2,10 +2,12 @@ class Solution {
     public double solution(int[] arr) {
         double answer = 0;
         
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             answer += arr[i];
         }
+        
         answer = answer / arr.length;
+        
         return answer;
     }
 }
