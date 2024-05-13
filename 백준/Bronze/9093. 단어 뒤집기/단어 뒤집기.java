677261ea3f76213700cae2c2941e163bc1c7bd35
LEE JIEUN <112913242+Jieun1ee@users.noreply.github.com>
2024-05-14 01:26:00 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 1660 ms, Memory: 61456 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            String[] words = br.readLine().split(" ");
+            for (String word : words) {
+                StringBuilder sb = new StringBuilder(word);
+                System.out.print(sb.reverse() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
